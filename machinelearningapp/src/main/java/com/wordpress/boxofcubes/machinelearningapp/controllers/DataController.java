39a84fc22fe325e5a3bf0d42a807c5ced589b9fe
emilypry <emilypry@yahoo.com>
2021-01-07 17:46:14 -0600

    made initial DataController handler

---
+++ machinelearningapp/src/main/java/com/wordpress/boxofcubes/machinelearningapp/controllers/DataController.java
@@ -0,0 +1,19 @@
+package com.wordpress.boxofcubes.machinelearningapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DataController {
+    @GetMapping("home")
+    public String showHome(){
+        return "home";
+    }
+}
