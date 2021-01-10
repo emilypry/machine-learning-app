ebcbc5fa7a54a03cd7d14070e7c3d1fa9d750456
emilypry <emilypry@yahoo.com>
2021-01-10 10:14:25 -0600

    added attributes to Data class

--- machinelearningapp/src/main/java/com/wordpress/boxofcubes/machinelearningapp/models/Data.java
+++ machinelearningapp/src/main/java/com/wordpress/boxofcubes/machinelearningapp/models/Data.java
@@ -6,13 +6,22 @@ public class Data {
     private double[] x;
     private double[] y;
     private int numPoints;
+    private String datasetName;
+    private String xLabel;
+    private String yLabel;
+    private String itemLabel;
 
     public Data(){}
-    public Data(String name, double[] x, double[] y){
+    public Data(String name, double[] x, double[] y, String datasetName, String xLabel,
+                String yLabel, String itemLabel){
         this.name = name;
         this.x = x;
         this.y = y;
         this.numPoints = x.length;
+        this.datasetName = datasetName;
+        this.xLabel = xLabel;
+        this.yLabel = yLabel;
+        this.itemLabel = itemLabel;
     }
 
     public int getId(){
@@ -41,5 +50,32 @@ public class Data {
     }
     public int getNumPoints(){
         return numPoints;
+    }
+    public void setNumPoints(int numPoints){
+        this.numPoints = numPoints;
+    }
+    public String getDatasetName(){
+        return datasetName;
+    }
+    public void setDatasetName(String datasetName){
+        this.datasetName = datasetName;
+    }
+    public String getXLabel(){
+        return xLabel;
+    }
+    public void setXLabel(String xLabel){
+        this.xLabel = xLabel;
+    }
+    public String getYLabel(){
+        return yLabel;
+    }
+    public void setYLabel(String yLabel){
+        this.yLabel = yLabel;
+    }
+    public String getItemLabel(){
+        return itemLabel;
+    }
+    public void setItemLabel(String itemLabel){
+        this.itemLabel = itemLabel;
     }
 }
