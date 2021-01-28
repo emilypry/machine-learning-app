7742669f81d0ef77610931f40c8d38e35c1397e4
emilypry <emilypry@yahoo.com>
2021-01-28 14:52:19 -0600

    cleaned up chart servlet

--- machinelearningapp/src/main/java/com/wordpress/boxofcubes/servlets/ChartServlet.java
+++ machinelearningapp/src/main/java/com/wordpress/boxofcubes/servlets/ChartServlet.java
@@ -30,66 +30,6 @@ public class ChartServlet extends HttpServlet{
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
         System.out.println("Entered chart...");
 
-        // THIS SEEMS TO WORK TOO, BUT MIGHT AS WELL REQUEST ONLY ONCE no, got confused a few times, ugh
-        /*if(request.getSession().getAttribute("data") != null){
-            Data data = (Data)request.getSession().getAttribute("data");
-            System.out.println("there was a data attribute");
-
-            response.setContentType("image/png");
-            OutputStream outputStream = response.getOutputStream();
-
-            // Make the chart
-            JFreeChart chart = getChart(data);
-            ChartUtils.writeChartAsPNG(outputStream, chart, 700, 400); 
-        }*/
-
-        // THIS SEEMS TO BE WORKING!!!!!! NO - shows wrong set sometimes
-        // IF I HIT TAB TOO MANY TIMES WHEN ENTERING DATA, WILL SWITCH TO SAMPLE LIFE SET!!!!!!!
-        /*Data data = (Data)request.getSession().getAttribute("data");
-        System.out.println("data name: "+data.getName());
-        if(data != null){
-            System.out.println("data object for servlet");
-
-            response.setContentType("image/png");
-            OutputStream outputStream = response.getOutputStream();
-
-            // Make the chart
-            JFreeChart chart = getChart(data);
-            ChartUtils.writeChartAsPNG(outputStream, chart, 700, 400); 
-        }else{
-            System.out.println("NO data object for servlet");
-        }*/
-        // can't do this because I'll have to use it later!!! still doesn't work reliably anyway
-        // actully, might - just not if I accidentally do more tabs on enter
-        //request.getSession().removeAttribute("data");
-        //System.out.println("Removed attribute from session "+data);
-
-
-
-
-        // THIS SEEMS TO WORK, BUT IS CLUNKY - WOULD PREFER TO NOT DEAL WITH UUID
-        // BUT SEEMS ONLY ONE THAT WORKS CONSISTENTLY!!!!!!!!!!!!!!
-        // Get the data UUID from the URL
-        /*String dataUUID = request.getParameter("dataUUID");
-        if(dataUUID != null){
-            System.out.println("There's a data object for the chart");
-        
-            // Reconstruct the data object associated with that UUID
-            Data data = (Data)request.getSession().getAttribute(dataUUID);
-            if(data != null){
-                response.setContentType("image/png");
-                OutputStream outputStream = response.getOutputStream();
-
-                // Make the chart
-                JFreeChart chart = getChart(data);
-                ChartUtils.writeChartAsPNG(outputStream, chart, 700, 400); 
-            }else{
-                System.out.println("uuid was there but Data object not found");
-            }   
-        }else{
-            System.out.println("There's NO data object for the chart!!!!!!!!!");
-        }*/
-        
         // Get Data UUID
         String dataUUID = request.getParameter("dataUUID");
         if(dataUUID != null){
@@ -99,16 +39,6 @@ public class ChartServlet extends HttpServlet{
 
                 // If there are predictions, show the chart with predictions
                 if(predictions != null){
-                    
-                    /*Boolean retrieved = (Boolean)request.getSession().getAttribute("retrieved");
-                    if(retrieved != null && retrieved == true){
-                        System.out.println("DATASET HAS BEEN RETRIEVED");
-
-                        // Remove 'retrieved' from session
-                        request.getSession().removeAttribute("retrieved");
-                        System.out.println("Removed retrieved from session ");
-                    }*/
-
                     response.setContentType("image/png");
                     OutputStream outputStream = response.getOutputStream();
         
@@ -126,22 +56,7 @@ public class ChartServlet extends HttpServlet{
                     OutputStream outputStream = response.getOutputStream();
     
                     // Make the chart
-                    JFreeChart chart = null;
-                    // If retrieving the chart, no need to make data values
-                    /*Boolean retrieved = (Boolean)request.getSession().getAttribute("retrieved");
-                    if(retrieved != null && retrieved == true){
-
-                        chart = retrieveChart(data);
-
-                        System.out.println("DATASET HAS BEEN RETRIEVED");
-
-                        // Remove 'retrieved' from session
-                        request.getSession().removeAttribute("retrieved");
-                        System.out.println("Removed retrieved from session ");
-                    }else{*/
-                        chart = getChart(data);
-                    //}
-                    
+                    JFreeChart chart = getChart(data);
                     ChartUtils.writeChartAsPNG(outputStream, chart, 700, 400); 
                 }
             }else{
@@ -152,12 +67,9 @@ public class ChartServlet extends HttpServlet{
             request.getSession().removeAttribute(dataUUID);
             System.out.println("Removed UUID from session "+dataUUID);
 
-            
-
         }else{
             System.out.println("no uuid!");
         }
-
     }
 
     private XYDataset getDataset(Data data){
@@ -187,10 +99,6 @@ public class ChartServlet extends HttpServlet{
         plot.setChart(chart);
 
         return chart;   
-    }
-
-    private JFreeChart retrieveChart(Data data){
-
     }
 
     private JFreeChart getChartWithPredictions(Data data, double[] predictions){
