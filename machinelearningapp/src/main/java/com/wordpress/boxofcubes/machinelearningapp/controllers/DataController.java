c560de531292cdb12724e0aed4cce37b35460066
emilypry <emilypry@yahoo.com>
2021-02-01 14:35:03 -0600

    got rid of unnecessary imports for data controller

--- machinelearningapp/src/main/java/com/wordpress/boxofcubes/machinelearningapp/controllers/DataController.java
+++ machinelearningapp/src/main/java/com/wordpress/boxofcubes/machinelearningapp/controllers/DataController.java
@@ -1,22 +1,14 @@
 package com.wordpress.boxofcubes.machinelearningapp.controllers;
 
-import java.io.IOException;
-import java.net.http.HttpRequest;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import javax.naming.SizeLimitExceededException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
-import javax.validation.Valid;
 
 import com.wordpress.boxofcubes.machinelearningapp.data.DataRepository;
-import com.wordpress.boxofcubes.machinelearningapp.data.DataValueRepository;
 import com.wordpress.boxofcubes.machinelearningapp.data.SavingModelRepository;
 import com.wordpress.boxofcubes.machinelearningapp.models.Data;
-import com.wordpress.boxofcubes.machinelearningapp.models.DataValue;
 import com.wordpress.boxofcubes.machinelearningapp.models.LinearRegression;
 import com.wordpress.boxofcubes.machinelearningapp.models.Parameters;
 import com.wordpress.boxofcubes.machinelearningapp.models.SavingModel;
@@ -29,17 +21,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.Errors;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.support.SessionAttributeStore;
-import org.springframework.web.bind.support.SessionStatus;
-import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class DataController {
